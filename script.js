fcdc1a0ab2fc1cf760ba1207bf96f36de02683f6
MarcusKyung <Kyungmj@gmail.com>
2023-07-18 21:49:10 -0700

    add styling and additional brewery info

--- script.js
+++ script.js
@@ -20,11 +20,16 @@ async function fetchData() {
       const response = await fetch(`https://api.openbrewerydb.org/v1/breweries?by_dist=${latitude},${longitude}`);
       const data = await response.json();
       const closestBrewery = data[0].name;
+      const closestBreweryAddress = data[0].street;
+      const closestBreweryState = data[0].state;
+      const closestBreweryPostalCode = data[0].postal_code;
 
+      document.getElementById("infoCardDisplay").classList.add("infoCard")
       document.getElementById("closestBeer").innerHTML = closestBrewery;
+      document.getElementById("closestBeerAddress").innerHTML = closestBreweryAddress + ", " + closestBreweryState + closestBreweryPostalCode;
   } catch (error) {
       console.error("Error:", error);
-      document.getElementById("closestBeer").innerHTML = "Error fetching data.";
+      document.getElementById("closestBeer").innerHTML = "Geolocation must be enabled in your browser settings for this extension to work.";
   }
 }
 
